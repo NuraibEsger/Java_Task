2e5c3441f9e92558067d215850fa78f5142b23a6
nuraibesger <nuraibesger@gmail.com>
2025-12-26 16:35:37 +0400

    Enum task has done

---
+++ src/TrafficLight.java
@@ -0,0 +1,5 @@
+public enum TrafficLight {
+    Red,
+    Green,
+    Yellow,
+}
--- src/task2/Monkey.java
+++
@@ -1,8 +0,0 @@
-package task2;
-
-public class Monkey extends Animal {
-    @Override
-    public void makeSound() {
-        System.out.println("task2.Monkey sound");
-    }
-}
