98516d3845477f4d88125bc3ebc555876570b835
nuraibesger <nuraibesger@gmail.com>
2025-12-30 16:41:51 +0400

    Task 2 done

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,18 @@ void main() {
     } catch(ArithmeticException e){
         System.out.println(e.getMessage());
     }
+
+    // Task 2
+
+    int[] arr = {1, 2, 3, 4, 5};
+
+    try{
+        for(int i = 0; i <= arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    } catch(ArrayIndexOutOfBoundsException e){
+        System.out.println(e.getMessage());
+    }
 }
 
 private int DivideByZero(int a, int b) {
