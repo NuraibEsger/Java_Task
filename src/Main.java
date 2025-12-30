d1e992fe915ee299965aa11a45a1025e5925b2c2
nuraibesger <nuraibesger@gmail.com>
2025-12-30 16:37:02 +0400

    Task 1 done

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     // Task 1
-    Calculator addition = (a, b) -> a+b;
-    Calculator multiplication = (a, b) -> a*b;
+    Scanner sc = new Scanner(System.in);
 
-    int result1 = addition.operate(5, 3);
-    int result2 = multiplication.operate(5, 4);
+    int num1 = sc.nextInt();
+    int num2 = sc.nextInt();
 
-    System.out.println(result1);
-    System.out.println(result2);
-
-    // Task 2
-    checkDay(Day.Monday);
-    checkDay(Day.Sunday);
-
-    // Task 3
-    TrafficLight currentTrafficLight = TrafficLight.Red;
-
-    switch (currentTrafficLight) {
-        case Red:
-            System.out.println("Red light");
-            break;
-        case Yellow:
-            System.out.println("Yellow light");
-            break;
-        case Green:
-            System.out.println("Green light");
-            break;
+    try{
+        int res = DivideByZero(num1,num2);
+        System.out.println(res);
+    } catch(ArithmeticException e){
+        System.out.println(e.getMessage());
     }
 }
 
-public static void checkDay(Day day) {
-    if (day == Day.Saturday || day == Day.Sunday) {
-        System.out.println(day + " is a Weekend.");
-    } else {
-        System.out.println(day + " is a Weekday.");
-    }
+private int DivideByZero(int a, int b) {
+    return a / b;
 }
