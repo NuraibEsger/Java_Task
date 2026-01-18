58c7d81145ab0692caa409a6e9a7e9bb025c4f2c
nuraibesger <nuraibesger@gmail.com>
2026-01-18 22:32:07 +0400

    Steam API task 4 done

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,25 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) {
         // Task 1
         List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
-        list.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
+        list.stream().filter(x -> x % 2 == 0).forEach(System.out::print);
+        System.out.println();
 
         // Task 2
         List<String> names = List.of("java","stream","api");
-        names.stream().map(String::toUpperCase).forEach(System.out::println);
+        names.stream().map(String::toUpperCase).forEach(System.out::print);
+        System.out.println();
 
         // Task 3
         List<Integer> nums =  List.of(5, 10, 15, 20);
-        nums.stream().filter(x -> x > 10).forEach(System.out::println);
+        nums.stream().filter(x -> x > 10).forEach(System.out::print);
 
-
+        // Task 4
+        List<String> data = Arrays.asList("A",null,"B",null,"C");
+        data.stream().filter(Objects::nonNull).forEach(System.out::print);
     }
 }
