43905cee1eaaccce949ba94b6dcd7e09f16ca6d2
nuraibesger <nuraibesger@gmail.com>
2026-01-18 22:27:51 +0400

    Steam API task 2 done

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,9 @@ public class Main {
         // Task 1
         List<Integer> list = List.of(1, 2, 3, 4, 5, 6);
         list.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
+
+        // Task 2
+        List<String> names = List.of("java","stream","api");
+        names.stream().map(String::toUpperCase).forEach(System.out::println);
     }
 }
