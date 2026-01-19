138230d12fde13541b3c5ac703e803fafcc52638
nuraibesger <nuraibesger@gmail.com>
2026-01-19 18:50:28 +0400

    Steam API task 8,9,10 done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Main {
@@ -37,5 +36,23 @@ public class Main {
         List<Integer> numbersForSum = List.of(1, 2, 3, 4, 5, 6);
         int sum = numbersForSum.stream().filter(x -> x % 2 == 0).mapToInt(Integer::intValue).sum();
         System.out.println(sum);
+
+        // Task 8
+        List<String> words = List.of("apple","banana","kiwi");
+        Map<String, Integer> mappedWords = words
+                .stream()
+                .collect(Collectors.toMap(word -> word, String::length));
+        System.out.println(mappedWords);
+
+        // Task 9
+        List<Integer> maxNumbers = List.of(10,50,30);
+        maxNumbers.stream().max(Integer::compareTo).ifPresent(System.out::println);
+
+        // Task 10
+        List<String> groupWords = List.of("a","bb","ccc","dd");
+        Map<Integer, List<String>> groupedByLength = groupWords
+                .stream()
+                .collect(Collectors.groupingBy(String::length));
+        System.out.println(groupedByLength);
     }
 }
